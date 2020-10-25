82792c15fba2841a028e7a110452871e45a45456
Henrik Samal <hsamal@gmail.com>
2020-10-25 10:23:40 +0200

    Add email to shop (#1)

--- src/main/java/com/shopify/model/Shop.java
+++ src/main/java/com/shopify/model/Shop.java
@@ -1,12 +1,19 @@
 package com.shopify.model;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement
+@XmlRootElement()
 public class Shop {
 
 	private String id;
 	private String name;
+	private String email;
+
+	@XmlAnyElement(lax = true)
+	private List<Object> anything;
 
 	public String getId() {
 		return id;
@@ -24,4 +31,19 @@ public class Shop {
 		this.name = name;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Object> getAnything() {
+		return anything;
+	}
+
+	public void setAnything(List<Object> anything) {
+		this.anything = anything;
+	}
 }
